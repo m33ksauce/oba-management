431745f9418ff1f936b3a4b71b141a202d25c224
Chris Meek <m33ksauce@gmail.com>
2022-03-03 21:11:55 -0600

    Add media controller and media service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MediaService } from './services/media/media.service';
+import { MediaController } from './controllers/media/media.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MediaController],
+  providers: [AppService, MediaService],
 })
 export class AppModule {}
