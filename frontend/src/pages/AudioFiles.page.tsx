3b6470bcef528649d8a0d2278041286513961d9b
Chris Meek <m33ks@sauce@gmail.com>
2022-03-11 16:22:10 -0600

    Add buttons for upload modal

--- frontend/src/pages/AudioFiles.page.tsx
+++ frontend/src/pages/AudioFiles.page.tsx
@@ -1,20 +1,46 @@
-import { Button, Col, Container, Row, Table } from "react-bootstrap"
+import React from "react";
+import { FaFileUpload, FaFolderPlus } from "react-icons/fa";
+import { Button, Col, Container, Modal, Row, Table } from "react-bootstrap"
+
+const FileUploadModal = (props: any) => {
+  return (
+    <Modal
+      show={props.show}
+      onHide={props.onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+
+      </Modal.Header>
+    </Modal>
+  )
+}
 
 export const AudioFilesPage = () => {
-    return (
-      <Container>
-        <Row>
-          <Col lg={{ span: 6, offset: 3}}>
-            <h1>Audio Files</h1><hr />
+  const [modalShow, setModalShow] = React.useState(false);
 
-            <Table hover>
-                <tr>
-                    <td>Luke 1:1-4</td>
-                    <td><Button>Remove</Button></td>
-                </tr>
-            </Table>
-          </Col>
-        </Row>
-      </Container>
+  return (
+    <Container>
+      <Row>
+        <Col lg={{ span: 6, offset: 3}}>
+          <h1>Audio Files</h1>
+          <FileUploadModal
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+            />
+          <hr />
+          <Button className="float-end" style={{marginLeft: "4px"}} variant="primary" onClick={() => setModalShow(true)}><FaFolderPlus /></Button>
+          <Button className="float-end" variant="primary" onClick={() => setModalShow(true)}><FaFileUpload /></Button>
+          <Table hover>
+              <tr>
+                  <td>Luke 1:1-4</td>
+                  <td><Button variant="primary">Remove</Button></td>
+              </tr>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
     )
-  }
+}
