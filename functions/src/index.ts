d7b605ec88dbf92b2735e841b9b6f04ef528896e
Chris Meek <m33ks@sauce@gmail.com>
2022-04-25 23:02:12 -0500

    Add cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,17 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as cors from "cors";
 import apiRouter from "./controllers/api/v1";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cors());
 
 app.use('/api/v1', apiRouter);
 
