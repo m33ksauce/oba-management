9decb3ed0dd8e8de98e500e590daa6fe159f274c
Chris Meek <m33ksauce@gmail.com>
2022-10-22 23:48:03 -0500

    Update docker config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 const express = require("express");
+import * as expressLib from "express";
 const bodyParser = require("body-parser");
 const cors = require("cors");
 import apiRouter from "./controllers/api/v1";
@@ -13,9 +14,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors({origin: true}));
 
+app.get("/health", (req: expressLib.Request, res: expressLib.Response) => {
+  res.sendStatus(200);
+});
 app.use("/api/v1", apiRouter);
 
 // export const handler = functions.https.onRequest(app);
-app.listen(8080, () => {
-    console.log(`Example app listening on port ${8080}`)
+app.listen(80, () => {
+    console.log(`Example app listening on port ${80}`)
   })
