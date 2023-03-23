ed2d90b6a70941c8c1a382f75f141a59466009bc
jpansch <jordan@xuppay.com>
2023-03-23 11:17:28 -0500

    add signup page

--- frontend_2/src/app/pages/sign-up/sign-up.page.ts
+++ frontend_2/src/app/pages/sign-up/sign-up.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+
 @Component({
-  selector: 'app-login',
-  templateUrl: 'login.page.html',
-  styleUrls: ['login.page.scss'],
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.page.html',
+  styleUrls: ['./sign-up.page.scss'],
 })
-export class LoginPage {
+export class SignUpPage {
   error = '';
 
   form: FormGroup;
@@ -16,15 +16,25 @@ export class LoginPage {
 
   passwordControl: FormControl;
 
+  nameControl: FormControl;
+
+  translationControl: FormControl;
+
   formSubmitting = false;
 
-  constructor(private router: Router, private authService: AuthenticationService) {
+  signupReceived = false;
+
+  constructor(private authService: AuthenticationService) {
     this.userNameControl = new FormControl('', [Validators.required, Validators.email]);
     this.passwordControl = new FormControl('', Validators.required);
+    this.nameControl = new FormControl('', Validators.required);
+    this.translationControl = new FormControl('', Validators.required);
 
     this.form = new FormGroup({
       userNameControl: this.userNameControl,
       passwordControl: this.passwordControl,
+      nameControl: this.nameControl,
+      translationControl: this.translationControl,
     });
   }
 
@@ -36,10 +46,6 @@ export class LoginPage {
 
     // this.formSubmitting = true;
 
-    this.router.navigate(['/home/translation']);
-  }
-
-  navigateToSignUp() {
-    this.router.navigate(['/signup']);
+    this.signupReceived = true;
   }
 }
