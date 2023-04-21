022b09e8496824f281a5861dabf339ef68bd1ec4
jpansch <jpanscho@gmail.com>
2023-04-21 15:18:42 -0500

    fix issue with user dropdown and editing items

--- frontend_2/src/app/shared/shared.module.ts
+++ frontend_2/src/app/shared/shared.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { ActiveUserComponent } from '../components/active-user/active-user.component';
+import { CurrentUserDropdownComponent } from '../components/current-user-dropdown/current-user-dropdown.component';
 
 @NgModule({
-  declarations: [ActiveUserComponent],
+  declarations: [CurrentUserDropdownComponent],
   imports: [CommonModule, IonicModule, FormsModule],
-  exports: [ActiveUserComponent],
+  exports: [CurrentUserDropdownComponent],
 })
 export class SharedModule {}
