0b5ab52a59e61f354b342a4b9eee54b51142049b
Chris Meek <m33ksauce@gmail.com>
2023-06-20 13:02:01 -0700

    Add prod build

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   api: {
-    url: 'https://content.oralbible.app/api/v1',
+    url: 'https://content.oralbible.app',
+    apiEndpoint: '/api/v1',
+    authEndpoint: '/auth'
   },
 };
